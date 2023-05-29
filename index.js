7b4f2749cc378b0fb5a6734dfe7d7e229c7657c6
Damien Bezborodov <dbezborodov@gmail.com>
2023-05-29 20:57:05 +0930

    elementLoadedById().

---
+++ index.js
@@ -0,0 +1,29 @@
+export default function elementLoadedById(id) {
+  return new Promise((resolve, reject) => {
+    const firstPass = document.getElementById(id);
+    if (firstPass) {
+      resolve(firstPass);
+      return;
+    }
+
+    const observer = new MutationObserver((_) => {
+      const element = document.getElementById(id);
+      if (element) {
+        observer.disconnect();
+        resolve(element);
+      }
+    });
+
+    document.addEventListener('DOMContentLoaded', () => {
+      reject('DOM finished loading, not found.');
+      observer.disconnect();
+    });
+
+    if (document.readyState != 'loading') {
+      reject('DOM already loaded, not found.');
+      return;
+    }
+
+    observer.observe(document, { subtree: true, childList: true });
+  });
+}
